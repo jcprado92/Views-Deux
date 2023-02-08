ce8b641713b938ba71bdee1b811e5ac3fca3e9f7
John Prado <jcprado20@icloud.com>
2023-02-08 18:38:46 -0500

    adds some sass styling

--- src/Components/Views/ViewDetail.js
+++ src/Components/Views/ViewDetail.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 function ViewDetail() {
+
+  const [ view, setView ] = useState([]);
+  const { id } = useParams();
+  
+  useEffect(() =>{
+    axios.get(`http://localhost:8080/views/${id}`)
+    .then(res => setView(res.data.payload))
+    .catch(err => console.log(err)) 
+  }, [])
+
+  const { name, url, location, is_favorite } = view;
   return (
-    <div>
-        <h1>Peep some deets</h1>
+    <div className='ViewDetail'>
+        <h1>Peep Some Deets</h1>
+        <p>{name}</p>
+        <img src={url}/>
+        <p>{location}</p>
+        <h4>{is_favorite ? "🌊What a View 🌊" : ""}</h4>
+        
+
     </div>
   )
 }
