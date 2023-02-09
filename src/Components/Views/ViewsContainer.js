730ed9085aa3e8b7d84adbaaee945224bd4e4d3a
John Prado <jcprado20@icloud.com>
2023-02-08 22:28:58 -0500

    adds detail component

--- src/Components/Views/ViewsContainer.js
+++ src/Components/Views/ViewsContainer.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import ViewCard from '../Views/ViewCard';
-import './ViewsContainer.scss'
-// const API = process.env.REACT_APP_API_URL;
 
+import '../Views/ViewsContainer.scss'
+
+// const API = process.env.REACT_APP_API_URL;
 function ViewsContainer() {
   const [views, setViews] = useState([]);
 
@@ -17,8 +18,7 @@ function ViewsContainer() {
 
   return (
     <div className="viewsContainer">
-      {/* <h1>These are some views</h1> */}
-      {views.map(view => <ViewCard view={view} />)}
+      {views.map(view => <ViewCard key={view.id} view={view} id={view.id}/>)}
     </div>
   );
 }
