da4daed63f0219b09e6829d8754f19e44f0470d5
John Prado <jcprado20@icloud.com>
2023-03-06 20:56:36 -0500

    adds mobile nav

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -29,19 +29,19 @@ const Nav = () => {
 };
 
 export default Nav;
-=======
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../Nav/nav.scss'
-function Nav() {
-  return (
-    <div className='nav'>
-        <Link to="/">Views</Link>
-        <Link to="/views">My Views</Link>
-        <Link to="/views/new">Add New View</Link>
-    </div>
-  )
-}
 
-export default Nav
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+// import '../Nav/nav.scss'
+// function Nav() {
+//   return (
+//     <div className='nav'>
+//         <Link to="/">Views</Link>
+//         <Link to="/views">My Views</Link>
+//         <Link to="/views/new">Add New View</Link>
+//     </div>
+//   )
+// }
+
+// export default Nav
 
